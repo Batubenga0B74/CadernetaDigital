c6c40b603254892c6cfcda70449bbac4c8a05ba2
batubenga <freudymena84@gmail.com>
2024-04-21 14:20:57 +0100

    tentando fazer a Validação

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log('oba')
+ export function logar(){
+    var texto = document.getElementById(cod)
+    var senha = document.getElementById(senhas)
+
+    if(texto == 'Admin' && senha == 'Admin'){
+        alert('foiii')
+    }
+}
